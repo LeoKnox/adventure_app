771bc4ae263042ebb2dad4d52385498c129ddf98
LeoKnox <LeoKnox@users.noreply.github.com>
2019-01-03 19:20:25 -0800

    Add files via upload
    Reads database information to display on webpage

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ var app = express();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db){
+MongoClient.connect(url, function(err, db) {
     if (err) throw err;
-    console.log("Database created!");
-    db.close();
+    var dbo = db.db("mydb");
+    var myobj = {name:"Giger", atk:"15", def:"15"};
+    dbo.collection("chars").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log('dbo');
+        db.close();
+    });
 });
 
 app.set('view engine', 'pug');
@@ -22,8 +27,18 @@ app.get('/', function(req, res) {
 });
 
 app.post('/run', function(req, res) {
-    res.render('form');
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("chars").findOne({_id: 0}, function(err, result) {
+            if (err) throw err;
+            console.log(result.name);
+            res.render('form', {result:result});
+            db.close();
+        });
+    });
     //res.send('You sent ' + req.body.login + ' and ' + req.body.pass);
+    //res.render('form', {result:result});
 });
 
 app.listen(3000);
